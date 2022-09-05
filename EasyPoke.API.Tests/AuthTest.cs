f0bbe7e5a959dcf4f676f1570f3eb7b613ef1a6b
Luth Andyka <luthandyka.business@gmail.com>
2022-09-05 18:48:31 +0800

    Create API Key Authentication tests

---
+++ EasyPoke.API.Tests/AuthTest.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace EasyPoke.API.Tests;
+
+public class AuthTest
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    private const string _apiKey = "secret";
+
+    public AuthTest()
+    {
+        _factory = new WebApplicationFactory<Program>();
+    }
+
+    [Theory]
+    [InlineData("/api/Auth")]
+    public async Task GetAuthCheck_WithoutApiKey_ReturnUnauthorized(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        Assert.Equal(response.StatusCode,
+                     HttpStatusCode.Unauthorized);
+    }
+
+    [Theory]
+    [InlineData("/api/Auth")]
+    public async Task GetAuthCheck_WithApiKey_ReturnOk(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("ApiKey", _apiKey);
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        Assert.Equal(response.StatusCode,
+                     HttpStatusCode.OK);
+    }
+}
