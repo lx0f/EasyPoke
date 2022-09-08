199afbeb3e35294af11cdd021180199e04e2b398
Luth Andyka <luthandyka.business@gmail.com>
2022-09-08 12:14:30 +0800

    Created GetUserByEmail UTs for UserService

--- EasyPoke.API.Tests/Systems/Services/TestUserService.cs
+++ EasyPoke.API.Tests/Systems/Services/TestUserService.cs
@@ -106,4 +106,100 @@ public class TestUserService
         // Arrange
         result.Should().BeFalse();
     }
+
+    [Fact]
+    public void GetUserByUsername_OnSuccess_ReturnUser()
+    {
+        // Arrange
+        string username = "testuser";
+
+        List<User> mockUserDbSet = new()
+        {
+            new()
+            {
+                Id = 1,
+                Username = username,
+                Email = "testuser@example.com",
+                Password = "12345678Aa"
+            }
+        };
+
+        var mockUserRepository = new Mock<IUserRepository>();
+        mockUserRepository
+            .Setup(repo => repo.GetUserByUsername(It.IsAny<string>()))
+            .Returns((string username) => mockUserDbSet.Single(u => u.Username == username));
+        var userService = new UserService(mockUserRepository.Object);
+
+        // Act
+        var result = userService.GetUserByUsername(username);
+
+        // Assert
+        result.Should().BeOfType<User>();
+        if (result != null)
+            result.Username.Should().Be(username);
+    }
+
+    [Fact]
+    public void GetUserByEmail_OnSuccess_ReturnUser()
+    {
+        // Arrange
+        string email = "testuser@example.com";
+
+        List<User> mockUserDbSet = new()
+        {
+            new()
+            {
+                Id = 1,
+                Username = "testuser",
+                Email = email,
+                Password = "12345678Aa"
+            }
+        };
+
+        var mockUserRepository = new Mock<IUserRepository>();
+        mockUserRepository
+            .Setup(repo => repo.GetUserByEmail(It.IsAny<string>()))
+            .Returns((string email) => mockUserDbSet.Single(u => u.Email == email));
+        var userService = new UserService(mockUserRepository.Object);
+
+        // Act
+        var result = userService.GetUserByEmail(email);
+
+        // Assert
+        result.Should().BeOfType<User>();
+        if (result != null)
+            result.Email.Should().Be(email);
+    }
+
+    [Fact]
+    public void ValidatePassword_OnSuccess_ReturnTrue()
+    {
+        // Arrange
+        string password = "12345678Aa";
+
+        var mockUserRepository = new Mock<IUserRepository>();
+        var userService = new UserService(mockUserRepository.Object);
+
+        // Act
+        var result = userService.ValidatePassword(password);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ValidatePassword_WithWeakPassword_ReturnFalse()
+    {
+        // Arrange
+        string password = "abcderf";
+
+        var mockUserRepository = new Mock<IUserRepository>();
+        var userService = new UserService(mockUserRepository.Object);
+
+        // Act
+        var result = userService.ValidatePassword(password);
+
+        // Assert
+        result.Should().BeFalse();
+    }
 }
