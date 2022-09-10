2e4626d639cf80cc8d415f846ffe73360624a04e
Luth Andyka <luthandyka.business@gmail.com>
2022-09-10 18:38:26 +0800

    Add Pokemon model

--- EasyPoke.API/Models/Pokemon.cs
+++ EasyPoke.API/Models/Pokemon.cs
@@ -1,6 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EasyPoke.API.Models;
 
 public class Pokemon
 {
-    
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int HitPoint { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int SpecialAttack { get; set; }
+    public int SpecialDefense { get; set; }
+    public int Speed { get; set; }
+
+    public PokemonType Type { get; set; }
+    public GrowthRate GrowthRate { get; set; }
 }
