e61da939ae95b9931d2c5283960a64766878f778
Luth Andyka <luthandyka.business@gmail.com>
2022-09-10 18:35:19 +0800

    Add GrowthRate Model

--- EasyPoke.API/Models/GrowthRate.cs
+++ EasyPoke.API/Models/GrowthRate.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EasyPoke.API.Models;
 
 public class GrowthRate
 {
-    
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Formula { get; set; }
+
+    public ICollection<GrowthRateLevelExperience> LevelExperiences { get; set; }
 }
--- EasyPoke.API/Models/GrowthRateLevelExperience.cs
+++ EasyPoke.API/Models/GrowthRateLevelExperience.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EasyPoke.API.Models;
 
 public class GrowthRateLevelExperience
 {
-    
+    [Key]
+    public int Id { get; set; }
+    public int Level { get; set; }
+    public int Experience { get; set; }
+
+    public GrowthRate ParentGrowthRate { get; set; }
 }
