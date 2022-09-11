db16b32d04e19dc611b7abf1be004b3cc4d83537
Luth Andyka <luthandyka.business@gmail.com>
2022-09-11 22:57:56 +0800

    Remove circular reference bug

--- EasyPoke.API/Models/GrowthRateLevelExperience.cs
+++ EasyPoke.API/Models/GrowthRateLevelExperience.cs
@@ -8,6 +8,4 @@ public class GrowthRateLevelExperience
     public int Id { get; set; }
     public int Level { get; set; }
     public int Experience { get; set; }
-
-    public GrowthRate ParentGrowthRate { get; set; }
 }
